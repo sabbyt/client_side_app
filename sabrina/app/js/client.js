e554aa3930e4872b79a258ae9209001ca7d6ecbb
SABRINA TEE <sabrinatee@me.com>
2016-02-11 14:04:15 -0800

    Working clock

--- sabrina/app/js/client.js
+++ sabrina/app/js/client.js
@@ -3,19 +3,16 @@ const moment = require('moment');
 const timeApp = angular.module('timeApp', []);
 
 timeApp.controller('timeController', ['$scope', '$interval', ($scope, $interval) => {
-  var currTime = moment(new Date());
+  var currTime;
   var timeZone = parseInt(moment().format('Z'));
+  $scope.count = 0;
 
   $interval(() => {
-    updateTime();
+    currTime = moment(new Date());
+    currTime.add($scope.count, 'hours').hours();
     $scope.time = currTime.format('MMMM Do YYYY, h:mm:ss a');
     $scope.timezone = timeZone;
   }, 1000);
-
-  function updateTime() {
-    currTime.add($scope.count, 'hours');
-    $scope.count = 0;
-  }
 
   $scope.addHour = () => {
     $scope.count++;
